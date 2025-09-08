7cfc0fe91bc043888d06c61fdf361cebe6e20f79
Richard Russon (DEPLOY) <rich@flatcap.org>
2025-09-08 03:35:55 +0000

    [AUTO] NeoMutt 20250905-2-g4bf191

--- ctype2_8h.js
+++ ctype2_8h.js
@@ -1,11 +1,11 @@
 var ctype2_8h =
 [
-    [ "mutt_isalnum", "ctype2_8h.html#ab5a9b59049a28f50eff234ed7a7952b3", null ],
-    [ "mutt_isalpha", "ctype2_8h.html#a2028ef193ad4fd1e66780577578fc79c", null ],
-    [ "mutt_isdigit", "ctype2_8h.html#a0cd4b325504a94116b30c7f2db4dff73", null ],
-    [ "mutt_ispunct", "ctype2_8h.html#ac704e44a92af81642364d4851b20828b", null ],
-    [ "mutt_isspace", "ctype2_8h.html#a4437ff43159c7036db44b6c18c494679", null ],
-    [ "mutt_isxdigit", "ctype2_8h.html#a5d500524a9accd9641b1403a90d29943", null ],
+    [ "mutt_isalnum", "ctype2_8h.html#abaec851ee8713f4c270906482c4041e5", null ],
+    [ "mutt_isalpha", "ctype2_8h.html#a3e00d052b6b7b8d7929168ecab667067", null ],
+    [ "mutt_isdigit", "ctype2_8h.html#adb914ebc2b43bd8f37f9684820e77867", null ],
+    [ "mutt_ispunct", "ctype2_8h.html#a5c8952525332bbdde5c9dd47ab274511", null ],
+    [ "mutt_isspace", "ctype2_8h.html#a16dd3da88a22f7e3ea99e3474fc46fda", null ],
+    [ "mutt_isxdigit", "ctype2_8h.html#a97d3f378a406a46a3e8707cada7bbbb3", null ],
     [ "mutt_tolower", "ctype2_8h.html#ad14cca3d15a5c91eceb6965fcff34cb9", null ],
     [ "mutt_toupper", "ctype2_8h.html#ab236bfcb8912f990e33888a8c8f16e1d", null ]
 ];
--- ctype_8c.js
+++ ctype_8c.js
@@ -1,11 +1,11 @@
 var ctype_8c =
 [
-    [ "mutt_isalnum", "ctype_8c.html#ab5a9b59049a28f50eff234ed7a7952b3", null ],
-    [ "mutt_isalpha", "ctype_8c.html#a2028ef193ad4fd1e66780577578fc79c", null ],
-    [ "mutt_isdigit", "ctype_8c.html#a0cd4b325504a94116b30c7f2db4dff73", null ],
-    [ "mutt_ispunct", "ctype_8c.html#ac704e44a92af81642364d4851b20828b", null ],
-    [ "mutt_isspace", "ctype_8c.html#a4437ff43159c7036db44b6c18c494679", null ],
-    [ "mutt_isxdigit", "ctype_8c.html#a5d500524a9accd9641b1403a90d29943", null ],
+    [ "mutt_isalnum", "ctype_8c.html#abaec851ee8713f4c270906482c4041e5", null ],
+    [ "mutt_isalpha", "ctype_8c.html#a3e00d052b6b7b8d7929168ecab667067", null ],
+    [ "mutt_isdigit", "ctype_8c.html#adb914ebc2b43bd8f37f9684820e77867", null ],
+    [ "mutt_ispunct", "ctype_8c.html#a5c8952525332bbdde5c9dd47ab274511", null ],
+    [ "mutt_isspace", "ctype_8c.html#a16dd3da88a22f7e3ea99e3474fc46fda", null ],
+    [ "mutt_isxdigit", "ctype_8c.html#a97d3f378a406a46a3e8707cada7bbbb3", null ],
     [ "mutt_tolower", "ctype_8c.html#ad14cca3d15a5c91eceb6965fcff34cb9", null ],
     [ "mutt_toupper", "ctype_8c.html#ab236bfcb8912f990e33888a8c8f16e1d", null ]
 ];
